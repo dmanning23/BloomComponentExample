823e44e20d5b17af953b742cc736d22fafbcf083
Dan Manning <dmanning23@hotmail.com>
2018-11-19 10:31:00 -0600

    Set old rendertarget instead of null, added iOS project

--- BloomComponentExample.SharedProject/Game1.cs
+++ BloomComponentExample.SharedProject/Game1.cs
@@ -48,8 +48,10 @@ namespace BloomComponentExample
 		/// <param name="gameTime">Provides a snapshot of timing values.</param>
 		protected override void Update(GameTime gameTime)
 		{
+#if !__IOS__
 			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
 				Exit();
+#endif
 
 			// TODO: Add your update logic here
 
